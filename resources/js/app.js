8580f29267e4b5e48affa766e9d293f7922151c5
tarik817 <tarik817@gmail.com>
2018-09-30 22:30:39 +0300

    Init project.

---
+++ resources/js/app.js
@@ -0,0 +1,46 @@
+
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter);
+
+import App from './components/App'
+import Home from './components/Home/Home'
+import Hello from './components/Hello'
+import UsersIndex from './components/UsersIndex'
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/hello',
+            name: 'hello',
+            component: Hello,
+        },
+        {
+            path: '/users',
+            name: 'users.index',
+            component: UsersIndex,
+        },
+    ],
+});
+
+const app = new Vue({
+    el: '#app',
+    components: { App },
+    router,
+});
